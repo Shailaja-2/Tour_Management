3d6c68e32ec0652713f9a9018dc3856ca26ef983
Shailaja-2 <167770048+Shailaja-2@users.noreply.github.com>
2025-05-21 09:59:22 +0530

    env

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,7 @@ const connect = async () => {
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(cookieParser());
-app.use("/api/v1/auth", authRoute);
+app.use("auth", authRoute);
 app.use("/tours", tourRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/review", reviewRoute);
