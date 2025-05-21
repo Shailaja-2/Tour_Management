7731649aa323d52e47f10234f22eb6fdb6d2e437
Shailaja-2 <167770048+Shailaja-2@users.noreply.github.com>
2025-05-21 10:48:38 +0530

    auth

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -44,7 +44,7 @@ export const login = async (req, res) => {
 
     // create jwt token
     const token = jwt.sign({id: user._id, role: user.role}, process.env.JWT_SECRET_KEY, {
-      expiresIn: "15d",
+      expiresIn: "30d",
     });
 
     //set token in the browser cookies and send the response to the client
